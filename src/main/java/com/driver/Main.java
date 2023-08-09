0223418c590537c6399986f66681b04a6dc0d91d
Salik Anwar <infernosight@gmail.com>
2023-08-09 11:01:34 +0530

    1st

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+//        RWOnly obj = new RWOnly("Accio");
+//        System.out.println(obj.name);
+//        Erro: java: name has private access in com.driver.RWOnly
+
+        RWOnly obj = new RWOnly();
+        obj.setter("Accio");
+        System.out.println(obj.getter());
+
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name = "Salik";
+
+    public RWOnly() {
+
+    }
+
+    public void setter(String name){
+        this.name = name;
+    }
+    public String getter(){
+        return name;
+    }
 }
